39ad987840785ba8aa0f35b906ad227c145fee1a
Rhea Dsouza <rheadqs@gmail.com>
2021-10-03 23:28:35 +0530

    day21

--- Day21.py
+++ Day21.py
@@ -1,1 +1,14 @@
-print("234")
+
+def mountain(arr):
+    peakindex=0
+    peakvalue=arr[0]
+    for i in range(1, len(arr)):
+        if peakvalue<arr[i]:
+            peakvalue=arr[i]
+            peakindex=i
+            print(peakindex)
+
+arr = [0, 10, 5, 2] 
+arr = [0, 2, 1, 0]
+
+mountain(arr)
