f5723359f45515643a5c7df6f9ac50fdb46502f8
Rhea Dsouza <rheadqs@gmail.com>
2021-10-14 19:28:23 +0530

    day32

---
+++ day32.py
@@ -0,0 +1,10 @@
+print("Enter your name: ")
+name = input()
+print("Welcome",(name))
+
+print("\nWhat is your age?")
+age = input()
+if (age>="18"):
+    print("\nYes! You can vote.")
+else:
+    print("Woops! You cannot vote.")    
