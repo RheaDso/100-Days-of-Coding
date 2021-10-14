434f13eae155cc5dff596f6264e6206b5e453ff1
Rhea Dsouza <rheadqs@gmail.com>
2021-10-14 21:18:26 +0530

    day32

---
+++ day32_2.py
@@ -0,0 +1,10 @@
+print('What is your name?')
+name=input()
+print('Welcome', (name))
+
+print ('\nWhere is DBIT situated?')
+place=input()
+if (place=="mumbai"):
+    print("Correct!")
+else:
+    print("Woops! That is incorrect.")    
